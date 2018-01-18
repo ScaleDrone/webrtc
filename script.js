ea7375971b697ecae80671f0cc3da678a19f782c
herkyl <s.herkyl@gmail.com>
2018-01-18 19:42:47 +0200

    Update channel_id

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ if (!location.hash) {
 const roomHash = location.hash.substring(1);
 
 // TODO: Replace with your own channel ID
-const drone = new ScaleDrone('63o6Zfoz6yeAcJDG');
+const drone = new ScaleDrone('yiS12Ts5RdNhebyM');
 // Room name needs to be prefixed with 'observable-'
 const roomName = 'observable-' + roomHash;
 const configuration = {
